02c374e38726c6f4405240f26581dfab84f6aeb3
meghanasp0802 <119754518+meghanasp0802@users.noreply.github.com>
2026-03-02 22:31:36 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,6 +1,8 @@
 def call(String Project,String ImageTag, String dockerhubUser)
 {
   withCredentials([usernamePassword(credentialsId: 'dockerHubCred' , passwordVariable: 'dockerHubPass', usernameVariable:'dockerHubUser')])
-  bat "docker login -u ${dockerhubUser} -p ${dockerhubpass}"
-  bat "docker push ${dockerhubuser}/${Project}:${ImageTag}"
+  {
+  bat "docker login -u ${dockerhubUser} -p ${dockerhubPass}"
+  bat "docker push ${dockerhubUser}/${Project}:${ImageTag}"
+}
 }
